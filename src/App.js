791d39ff163f69eff1e38c8272366d562cb031ed
tenefra <stenefra@gmail.com>
2021-07-12 20:27:31 -0500

    update status

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import React from "react"
 import "./App.css"
 
-function App() {
-  return <div className="App">Hello ACA!</div>
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return <div className="App">Hello A!</div>
+  }
 }
+
+// function App() {
+//   return (
+//     <div className="App">Hello ACA!</div>
+//   )
+// }
 
 export default App
