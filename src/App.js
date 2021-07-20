a6e4002fdb3e198eea4424215b7ed3fc53e8fd44
tenefra <stenefra@gmail.com>
2021-07-19 23:53:06 -0500

    update with bugs

--- src/App.js
+++ src/App.js
@@ -4,10 +4,70 @@ import "./App.css"
 class App extends React.Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      isClicked: false,
+      todos: [],
+      text: "",
+    }
+  }
+
+  componentDidUpdate() {
+    console.log(this.state.text)
+  }
+
+  onClickHandler = () => {
+    this.setState({
+      isClicked: true
+    })
+    console.log(this.state.isClicked)
+    this.state.todos.push(this.state.text)
+    this.setState({
+      todos: [...this.state.todos, {id: this.state.todos.length + 1, text: this.state.text}],
+      isClicked: false,
+      text: ""
+    })
+  }
+
+  handleClick = (id) => {
+    console.log(id)
+    let foundIndex = this.state.todos.findIndex((todo) => {
+       return todo.id === id
+    })
+
+    const copy = {...this.state.todos}
+    copy.splice(foundIndex, 1)
+    this.setState({todos: copy})
+    console.log(foundIndex)
+  }
+
+
+  handleChange = event => {
+    this.setState({
+      text: event.target.value,
+      
+    })
   }
 
   render() {
-    return <div className="App">Hello A!</div>
+    return (
+      <div className="App">
+        <button onClick={this.onClickHandler}>Add todo</button>
+        <input type="text" onChange={this.handleChange} />
+        <div>
+          <ul>
+            {this.state.todos.map(todo => {
+              return (
+                <li>
+                  <h3>{todo}</h3>
+                   <button onClick={(evt) => this.handleClick(evt, id)}>Delete</button>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </div>
+    )
   }
 }
 
